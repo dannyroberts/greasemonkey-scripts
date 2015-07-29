9e85fd05a6d3031f0d745b8413b6eba88f7321df
Ethan Soergel <esoergel@dimagi.com>
2015-07-29 11:43:49 -0400

    Add Phillip's updates to github-ignore-whitespace

--- github-ignore-whitespace.user.js
+++ github-ignore-whitespace.user.js
@@ -6,22 +6,55 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-function main() {
-  $("#toc > .btn-group").prepend('<a class="btn btn-sm" href="?w=1">Ignore whitespace</a>');
+function createButton() {
+    if ($("#ignore-whitespace").length == 0) {
+        $("#toc > .btn-group").prepend('<a class="btn btn-sm" href="" id="ignore-whitespace"></a>');
+    }
+
+    $("#ignore-whitespace").click(updateButton);
+    updateButton();
+}
+
+function updateButton() {
+    var showWhitespace = $.getUrlVar("w");
+    if (showWhitespace == 1) {
+        $("#ignore-whitespace").attr('href', '?w=0');
+        $("#ignore-whitespace").text('Show whitespace');
+    } else {
+        $("#ignore-whitespace").attr('href', '?w=1');
+        $("#ignore-whitespace").text('Ignore whitespace');
+    }
 }
 
 window.addEventListener('load', function () {
-  if (typeof $ === 'undefined') {
-    // load jQuery : http://stackoverflow.com/a/4261831/10840
-    var script = document.createElement('script');
-    script.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
-    script.addEventListener('load', function () {
-      var script = document.createElement('script');
-      script.textContent = 'window.jQ=jQuery.noConflict(true);(' + main.toString() + ')();';
-      document.body.appendChild(script);
-    }, false);
-    document.body.appendChild(script);
-  } else {
-    main();
-  }
+    if (typeof $ === 'undefined') {
+        // load jQuery : http://stackoverflow.com/a/4261831/10840
+        var script = document.createElement('script');
+        script.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+        script.addEventListener('load', function () {
+            var script = document.createElement('script');
+            script.textContent = 'window.jQ=jQuery.noConflict(true);(' + main.toString() + ')();';
+            document.body.appendChild(script);
+        }, false);
+        document.body.appendChild(script);
+    } else {
+        createButton();
+    }
+});
+
+$.extend({
+    getUrlVars: function(){
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    },
+    getUrlVar: function(name){
+        return $.getUrlVars()[name];
+    }
 });
